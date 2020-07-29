6c075fc3f25293e00c3870a188eeb80130bb2b7f
Tomás Lopes <tomas050302@gmail.com>
2020-07-29 13:04:38 +0100

    feat(web): finished web timeline

--- web/src/components/Graph/index.tsx
+++ web/src/components/Graph/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Timeline, TimelineEvent } from 'react-event-timeline';
 
-const Graph: React.FC = () => {
-  return <div />;
+import { Container } from './styles';
+interface Props {
+  data: {
+    name: string;
+    dates: {
+      firstCommitDate: number;
+      lastCommitDate: number;
+    };
+  }[];
+}
+
+const Graph: React.FC<Props> = ({ data }) => {
+  return (
+    <Container>
+      {data[1] ? (
+        <Timeline orientation='right'>
+          {data.map(repository => (
+            <TimelineEvent
+              title={repository.name}
+              subtitle={`From ${new Date(
+                repository.dates.firstCommitDate
+              ).toDateString()} to ${new Date(
+                repository.dates.lastCommitDate
+              ).toDateString()}`}
+              createdAt={repository.dates.firstCommitDate}
+              key={repository.name}
+            ></TimelineEvent>
+          ))}
+        </Timeline>
+      ) : null}
+    </Container>
+  );
 };
 
 export default Graph;
--- web/src/components/Layout/index.tsx
+++ web/src/components/Layout/index.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Wrapper } from './styles';
+import {
+  Container,
+  Wrapper,
+  Inputs,
+  UsernameInput,
+  SubmitButton,
+} from './styles';
 
 import Header from '../Header';
-import Inputs from '../Inputs';
 import Graph from '../Graph';
 
+import api from '../../services/api';
+
 const Layout: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [data, setData] = useState([
+    {
+      name: '',
+      dates: {
+        firstCommitDate: Date.now(),
+        lastCommitDate: Date.now(),
+      },
+    },
+  ]);
+
+  async function handleSubmit() {
+    const response = await api.get(`/timeline/${username}`);
+
+    setData(response.data);
+    console.log(response.data);
+  }
+
   return (
     <Container>
       <Wrapper>
         <Header />
-        <Inputs />
-        <Graph />
+        <Inputs>
+          <UsernameInput
+            onChange={e => setUsername(e.target.value)}
+            placeholder='GitHub username'
+          />
+          <SubmitButton onClick={handleSubmit}>Submit</SubmitButton>
+        </Inputs>
       </Wrapper>
+      <Graph data={data} />
     </Container>
   );
 };
